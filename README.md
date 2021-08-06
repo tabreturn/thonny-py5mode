783adfbecfe5012527d785d7dc158328f884b9b3
tabreturn <github@tabreturn.com>
2021-08-06 13:49:46 +1200

    - add menu toggle; swap out sys.stdout for print

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ This plugin is designed to work with the portable version of Thonny. It's also l
 
 4. Once Thonny is open, select *Tools > Manage plug-ins...*, then search for and install __thonny-py5mode__ (note you'll need to restart Thonny after this step).
 
-5. Once you've restarted Thonny (from your terminal?), select *Tools > Activate py5 mode* (this'll download and extract JDK-11 into `thonny/bin/jdk-11` and set `JAVA_HOME` to that path).
+5. Once you've restarted Thonny (from your terminal?), select *Tools > py5 mode* (this'll download and extract JDK-11 into `thonny/bin/jdk-11` and set `JAVA_HOME` to that path).
 
 6. Try out a sketch (note I haven't programmed 'imported mode' support yet):
 
@@ -45,6 +45,7 @@ If you're interested in Python for creative coding and don't know about [hx2A's]
 ## todo list
 
 - ~~Get started~~
+- ~~Add tickable/toggled menu option~~
 - Display download/installation progress in Thonny (not the terminal)
 - Add support for [py5 imported mode](http://py5.ixora.io/tutorials/py5-modes/#imported-mode)
 - Buttons and indicators! How does one switch between modes using Thonny; how is this reflected in the interface?
--- thonnycontrib/thonny-py5mode/__init__.py
+++ thonnycontrib/thonny-py5mode/__init__.py
@@ -2,6 +2,10 @@
 thonny-py5mode.
 
 py5 plugin for Thonny.
+
+# NOTES TO SELF:
+* documentation: https://github.com/thonny/thonny/wiki/Plugins
+* maybe look at how Run > Pygame Zero Mode is programmed?
 '''
 
 __version__ = "0.0.1"
@@ -11,44 +15,47 @@ __credits__ = 'organisation name'
 
 import os, pathlib, sys
 # for jdk downloader (https://pypi.org/project/install-jdk/)
-import itertools, jdk, threading, time
+import jdk, threading, time
+# thonny plugin imports
+from thonny import get_workbench
+from thonny.languages import tr
+from tkinter.messagebox import showinfo
 
 
 def setJAVA_HOME():
-    # set jdk path to thonny/lib/jdk-11
+    ''' set jdk path to thonny/lib/jdk-11 '''
     jdk = pathlib.Path(sys.executable).parents[1] / 'lib' / 'jdk-11'
     os.environ['JAVA_HOME'] = str(jdk)
 
 
 def activatePy5():
-    # get thonny (portable version) paths
+    ''' get thonny (portable version) paths '''
     thonny_interpreter = pathlib.Path(sys.executable)
     thonny_root = pathlib.Path(thonny_interpreter).parents[1]
     thonny_lib = thonny_root / 'lib'
 
     if not pathlib.Path(thonny_lib / 'jdk-11').exists():
-        sys.stdout.write('\npy5 requires jdk-11')
-        sys.stdout.write('\nplease wait a moment while this installs\n')
-        
-        # progress indicator for downloading jdk
+        print('py5 requires jdk-11')
+
         def display_progress(task):
-            progress = '.'
+            ''' progress indicator for downloading jdk '''
+            print(msg)
+            progress = '> ·'
             while not progress_complete:
-                sys.stdout.write(task + progress)
-                sys.stdout.flush()
-                progress += '.'
-                time.sleep(0.1)
+                print(progress)
+                progress += '-'
+                time.sleep(1)
 
         # download jdk
         progress_complete = False
-        msg = '\r> downloading jdk '
+        msg = '> downloading jdk '
         dl_thread = threading.Thread(target=display_progress, args=(msg,))
         dl_thread.start()
         jdk_file = jdk._download(jdk.get_download_url('11'))
         progress_complete = True
 
         # extract jdk to thonny's thonny/lib/
-        sys.stdout.write('\n> extracting jdk')
+        print('> extracting jdk')
         jdk._USER_DIR = thonny_root
         jdk_ext = jdk._get_normalized_compressed_file_ext(jdk_file)
         jdk_dir = jdk._decompress_archive(jdk_file, jdk_ext, thonny_lib)
@@ -58,7 +65,7 @@ def activatePy5():
         for name in os.listdir(lib_path):
             if name.startswith('jdk-11'):
                 os.rename(lib_path / name, lib_path / 'jdk-11')
-                sys.stdout.write('\n> done!\n')
+                print('> done!')
                 setJAVA_HOME()
                 return 'JDK-11 installed and JAVA_HOME set'
 
@@ -68,21 +75,33 @@ def activatePy5():
         return 'JAVA_HOME set'
 
 
-# NOTES TO SELF:
-# documentation: https://github.com/thonny/thonny/wiki/Plugins
-# maybe look at how Run > Pygame Zero Mode is programmed?
+_OPTION_NAME = 'run.py5_mode'
 
 
-from thonny import get_workbench
-from tkinter.messagebox import showinfo
+def toggle_variable():
+    var = get_workbench().get_variable(_OPTION_NAME)
+    var.set(not var.get())
+    update_environment()
+
+    if var.get():
+        # activate py5 (and download jdk if necessary) 
+        showinfo(activatePy5(), 'py5 mode activated')
 
 
-def test():
-    showinfo('Py5 ready!', activatePy5())
+def update_environment():
+    if get_workbench().in_simple_mode():
+        os.environ['PY5_MODE'] = 'auto'
+    else:
+        os.environ['PY5_MODE'] = str(get_workbench().get_option(_OPTION_NAME))
 
 
 def load_plugin():
-    get_workbench().add_command(command_id="Activate py5 mode",
-                                menu_name="tools",
-                                command_label="Activate py5 mode",
-                                handler=test)
+    get_workbench().set_default(_OPTION_NAME, False)
+    get_workbench().add_command(
+      'toggle_py5_mode',
+      'tools',
+      tr('py5 mode'),
+      toggle_variable,
+      flag_name=_OPTION_NAME,
+      group=180,
+    )
