8b1cea6df7f3638a4f29636f3294d4c233e2f900
tabreturn <github@tabreturn.com>
2021-08-07 15:23:11 +1200

    - add module mode menu item

--- thonnycontrib/thonny-py5mode/__init__.py
+++ thonnycontrib/thonny-py5mode/__init__.py
@@ -11,26 +11,29 @@ py5 plugin for Thonny.
 import os
 import pathlib
 import shutil
+import subprocess
+import site
 import sys
-# for jdk downloader (https://pypi.org/project/install-jdk/)
 import threading
 import time
-import jdk
-# thonny plugin imports
-from thonny import get_workbench, THONNY_USER_DIR
-from thonny.languages import tr
 from tkinter.messagebox import showinfo
+import jdk
+from py5_tools import imported
+from thonny.languages import tr
+from thonny import get_workbench, THONNY_USER_DIR
+from thonny import running
+from thonny.ui_utils import select_sequence
 
 __author__ = 'tabreturn'
 __credits__ = 'organisation name'
 
 
-def set_java_home(jdk_path):
+def set_java_home(jdk_path: str) -> None:
     '''set jdk path to thonny/lib/jdk-11'''
     os.environ['JAVA_HOME'] = str(jdk_path)
 
 
-def activate_py5(install_type):
+def activate_py5(install_type: str):
     '''get thonny (portable version) paths'''
     require_jdk = 11
     jdk_dir = 'jdk-' + str(require_jdk)
@@ -52,7 +55,7 @@ def activate_py5(install_type):
             if name.startswith(jdk_dir):
                 shutil.rmtree(pathlib.Path(install_to) / name)
 
-        def display_progress(task):
+        def display_progress(task: str):
             '''progress indicator for downloading jdk'''
             print(task)
             progress = '·'
@@ -87,24 +90,24 @@ def activate_py5(install_type):
         set_java_home(pathlib.Path(install_to) / dest)
         return 'JDK-11 installed and JAVA_HOME set'
 
-    else:
-        # set jdk path to thonny app / config directory
-        set_java_home(pathlib.Path(install_to) / jdk_dir)
-        return 'JAVA_HOME set'
+    # set jdk path to thonny app / config directory
+    set_java_home(pathlib.Path(install_to) / jdk_dir)
+    return 'JAVA_HOME set'
 
 
 _OPTION_PORTABLE = 'run.py5_mode_portable'
 _OPTION_INSTALLED = 'run.py5_mode_installed'
 # workaround for .add_command() handler parameter that won't accept arguments
-def toggle_variable_portable(): toggle_variable('portable')
-def toggle_variable_installed(): toggle_variable('installed')
+def toggle_variable_portable() -> None: toggle_variable('portable')
+def toggle_variable_installed() -> None: toggle_variable('installed')
 
 
-def toggle_variable(install_type):
+var_portable = get_workbench().get_variable(_OPTION_PORTABLE)
+var_installed = get_workbench().get_variable(_OPTION_INSTALLED)
+
+
+def toggle_variable(install_type: str) -> None:
     '''install_type is portable or installed'''
-    var_portable = get_workbench().get_variable(_OPTION_PORTABLE)
-    var_installed = get_workbench().get_variable(_OPTION_INSTALLED)
-
     if install_type == 'portable':
         var_installed.set(False)
         var_portable.set(not var_portable.get())
@@ -113,37 +116,77 @@ def toggle_variable(install_type):
         var_portable.set(False)
         var_installed.set(not var_installed.get())
 
-    '''DON'T KNOW WHAT THIS IS FOR
-    if get_workbench().in_simple_mode():
-        os.environ['PY5_MODE'] = 'auto'
-    else:
-        os.environ['PY5_MODE'] = str(get_workbench().get_option(_OPTION_...))
-    '''
+    # NOTE: don't know what this is for
+    # if get_workbench().in_simple_mode():
+    #     os.environ['PY5_MODE'] = 'auto'
+    # else:
+    #     os.environ['PY5_MODE'] = str(get_workbench().get_option(_OPTION_...))
+
     if var_portable.get() or var_installed.get():
         # activate py5 (and download jdk if necessary)
-        showinfo(activate_py5(install_type), 'py5 mode activated')
+        activated_msg = 'py5 mode for %s Thonny activated' % install_type
+        showinfo(activate_py5(install_type), activated_msg)
 
 
-def load_plugin():
+def execute_module_mode() -> None:
+    '''
+    there's got to be a better approach than this ...
+    maybe something that employs a new Runner instance
+    maybe something that changes the thonny's default run behaviour
+    '''
+    current_editor = get_workbench().get_editor_notebook().get_current_editor()
+    current_file = current_editor.get_filename()
+
+    if current_file is None:
+        # thonny must 'save as' any new files, before it can run them
+        showinfo('py5 module mode error', 'Save your file somewhere first')
+
+    elif current_file and current_file.split('.')[-1] in ('py', 'py5', 'pyde'):
+        # save and run py5 module mode
+        current_editor.save_file()
+        run_sketch = '/py5_tools/tools/run_sketch.py'
+        run_sketch = str(site.getsitepackages()[0]) + run_sketch
+        working_directory = os.path.dirname(current_file)
+        cd_cmd_line = running.construct_cd_command(working_directory) + '\n'
+        cmd_parts = ['%Run', run_sketch, current_file]
+        ed_token = [running.EDITOR_CONTENT_TOKEN]
+        exe_cmd_line = running.construct_cmd_line(cmd_parts, ed_token) + '\n'
+        running.get_shell().submit_magic_command(cd_cmd_line + exe_cmd_line)
+
+
+def load_plugin() -> None:
     '''every thonny plug-in uses this function to load'''
+    var_installed.set(False)
+    var_portable.set(False)
     # portable button
     get_workbench().set_default(_OPTION_PORTABLE, False)
     get_workbench().add_command(
-        'toggle_py5_mode_portable',
-        'py5',
-        tr('py5 mode for portable Thonny'),
-        toggle_variable_portable,
-        flag_name=_OPTION_PORTABLE,
-        group=100
+      'toggle_py5_mode_portable',
+      'py5',
+      tr('py5 mode for portable Thonny'),
+      toggle_variable_portable,
+      flag_name=_OPTION_PORTABLE,
+      group=100
     )
     # non-portable / installed button
     get_workbench().set_default(_OPTION_INSTALLED, False)
     get_workbench().add_command(
-        'toggle_py5_mode',
-        'py5',
-        tr('py5 mode for installed Thonny'),
-        toggle_variable_installed,
-        flag_name=_OPTION_INSTALLED,
-        group=100
+      'toggle_py5_mode',
+      'py5',
+      tr('py5 mode for installed Thonny'),
+      toggle_variable_installed,
+      flag_name=_OPTION_INSTALLED,
+      group=100
+    )
+    # run link
+    # NOTE: perhaps this should be a toggle that in-turn affects the thonny run
+    get_workbench().add_command(
+      'execute_module_mode',
+      'py5',
+      'Execute using py5 module mode',
+      execute_module_mode,
+      default_sequence='<Control-u>',
+      extra_sequences=[select_sequence('<Control-Alt-u>', '<Command-u>')],
+      group=200
     )
 
