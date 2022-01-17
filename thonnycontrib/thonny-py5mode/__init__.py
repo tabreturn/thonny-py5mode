3641e44778453ab667bad18a0fef52051d055b88
tabreturn <github@tabreturn.com>
2022-01-18 10:57:04 +1300

    - fix jdk-path config

--- thonnycontrib/thonny-py5mode/__init__.py
+++ thonnycontrib/thonny-py5mode/__init__.py
@@ -10,8 +10,8 @@ from thonny.languages import tr
 _PY5_IMPORTED_MODE = 'run.py5_imported_mode'
 
 
-def update_environment() -> None:
-    '''set mode in configuration.ini file'''
+def set_py5_imported_mode() -> None:
+    '''set variable to switch on/off py5 run button behavior'''
     if get_workbench().in_simple_mode():
         os.environ['PY5_IMPORTED_MODE'] = 'auto'
     else:
@@ -19,11 +19,12 @@ def update_environment() -> None:
         os.environ['PY5_IMPORTED_MODE'] = p_i_m
 
 
-def toggle_variable() -> None:
-    '''toggle variables'''
+def activate_py5_imported_mode() -> None:
+    '''activate py5 imported mode settings'''
     var = get_workbench().get_variable(_PY5_IMPORTED_MODE)
+    # toggle variable (and menu checkbox)
     var.set(not var.get())
-    update_environment()
+    set_py5_imported_mode()
 
 
 def set_jdk_path() -> None:  # CALL THIS WHEN JDK IS INSTALLED <---
@@ -34,6 +35,8 @@ def set_jdk_path() -> None:  # CALL THIS WHEN JDK IS INSTALLED <---
     if jdk_path not in env_vars:
         env_vars.append(jdk_path)
 
+    get_workbench().set_option('general.environment', env_vars)
+
 
 def apply_recommended_py5_config() -> None:  # ADD A RESTORE (TO EARLIER SETTINGS) FEATURE <---
     '''apply some recommended settings for thonny py5 work'''
@@ -42,7 +45,6 @@ def apply_recommended_py5_config() -> None:  # ADD A RESTORE (TO EARLIER SETTING
     get_workbench().set_option('view.syntax_theme', 'Kyanite Syntax')
     get_workbench().set_option('assistance.open_assistant_on_errors', 'False')
     get_workbench().reload_themes()
-    update_environment()
 
 
 def load_plugin() -> None:
@@ -51,7 +53,7 @@ def load_plugin() -> None:
       'toggle_py5_imported_mode',
       'py5',
       tr('py5 imported mode'),
-      toggle_variable,
+      activate_py5_imported_mode,
       flag_name=_PY5_IMPORTED_MODE,
       group=10,
     )
@@ -62,4 +64,4 @@ def load_plugin() -> None:
       apply_recommended_py5_config,
       group=20,
     )
-    update_environment()
+    set_py5_imported_mode()
