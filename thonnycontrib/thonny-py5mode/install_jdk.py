4d858465b23701389760f09c49d72ea546986a66
tabreturn <github@tabreturn.com>
2022-07-07 17:38:26 +1200

    - refactor install_jdk code

--- thonnycontrib/thonny-py5mode/install_jdk.py
+++ thonnycontrib/thonny-py5mode/install_jdk.py
@@ -7,10 +7,10 @@ import os
 import pathlib
 import threading
 import tkinter as tk
-from tkinter import ttk
-from tkinter.messagebox import showinfo
 from thonny import get_workbench, THONNY_USER_DIR, ui_utils
 from thonny.languages import tr
+from tkinter import ttk
+from tkinter.messagebox import showinfo
 
 
 _REQUIRE_JDK = 17
@@ -18,61 +18,72 @@ _INSTALL_JDK_MESSAGE = '''Thonny requires JDK to run py5 sketches. \
 It\'ll need to download about 180 MB.'''
 
 
+def jdk_install_exists() -> bool:
+    '''check system for existing jdk that meets the py5 version requirements'''
+    jdk_dir = 'jdk-' + str(_REQUIRE_JDK)
+    system_jdk = 'TBD'
+
+    # check system for existing jdk install (and the version number)
+    if os.environ.get('JAVA_HOME') is not None:
+        jdk_ver = os.environ['JAVA_HOME'].split('jdk-')[-1].split('.')[0]
+        system_jdk = jdk_ver
+
+    for name in os.listdir(THONNY_USER_DIR):
+        # check for jdk in the thonny config directory
+        if name.startswith(jdk_dir):
+            # set jdk path to thonny config directory
+            set_java_home(pathlib.Path(THONNY_USER_DIR) / jdk_dir)
+            return True
+
+    if not system_jdk.isdigit() or int(system_jdk) < _REQUIRE_JDK:
+        return False
+
+
+def set_java_home(jdk_path: pathlib.Path) -> None:
+    '''add jdk path to config file (tools > options > general > env vars)'''
+    jdk_path = 'JAVA_HOME=' + str(jdk_path)
+    env_vars = get_workbench().get_option('general.environment')
+
+    if jdk_path not in env_vars:
+        env_vars.append(jdk_path)
+
+    get_workbench().set_option('general.environment', env_vars)
+
+
 class DownloadJDK(threading.Thread):
     def __init__(self):
         super().__init__()
 
-    def run(self) -> str:
+    def run(self) -> None:
         '''download and setup jdk (installs to thonny config directory)'''
         jdk_dir = 'jdk-' + str(_REQUIRE_JDK)
 
-        system_jdk = 'TBD'
-        # check system for existing jdk install (and the version number)
-        if os.environ.get('JAVA_HOME') is not None:
-            jdk_ver = os.environ['JAVA_HOME'].split('jdk-')[-1].split('.')[0]
-            system_jdk = jdk_ver
+        for name in os.listdir(THONNY_USER_DIR):
+            # delete existing thonny-py5mode jdk (if one exists)
+            if name.startswith(jdk_dir):
+                shutil.rmtree(pathlib.Path(install_to) / name)
+        # download jdk
+        print(f'> downloading jdk-{_REQUIRE_JDK}')
+        jdk_file = jdk._download(jdk.get_download_url(_REQUIRE_JDK))
+        # extract jdk
+        print('> extracting jdk')
+        jdk._USER_DIR = THONNY_USER_DIR
+        jdk_ext = jdk._get_normalized_compressed_file_ext(jdk_file)
+        jdk._decompress_archive(jdk_file, jdk_ext, THONNY_USER_DIR)
 
         for name in os.listdir(THONNY_USER_DIR):
-            # check for jdk in thonny config directory
+            # rename extracted jdk directory to jdk-<version#>
             if name.startswith(jdk_dir):
-                # set jdk path to thonny config directory
-                set_java_home(pathlib.Path(THONNY_USER_DIR) / jdk_dir)
-                return 'JAVA_HOME set'
-
-        if not system_jdk.isdigit() or int(system_jdk) < _REQUIRE_JDK:
-            print('> py5 requires jdk-%d' % _REQUIRE_JDK)
-
-            for name in os.listdir(THONNY_USER_DIR):
-                # delete existing thonny-py5mode jdk (if one exists)
-                if name.startswith(jdk_dir):
-                    shutil.rmtree(pathlib.Path(install_to) / name)
-            # download jdk
-            jdk_file = jdk._download(jdk.get_download_url(_REQUIRE_JDK))
-            # extract jdk
-            print('> extracting jdk')
-            jdk._USER_DIR = THONNY_USER_DIR
-            jdk_ext = jdk._get_normalized_compressed_file_ext(jdk_file)
-            jdk._decompress_archive(jdk_file, jdk_ext, THONNY_USER_DIR)
-
-            for name in os.listdir(THONNY_USER_DIR):
-                # rename extracted jdk directory to jdk-11
-                if name.startswith(jdk_dir):
-                    src = pathlib.Path(THONNY_USER_DIR) / name
-                    dest = pathlib.Path(THONNY_USER_DIR) / jdk_dir
-                    os.rename(src, dest)
-                    print('> done!')
-                    return 'All done! py5 is ready.'
-                    break
-
-            set_java_home(pathlib.Path(THONNY_USER_DIR) / dest)
-            # one-off env var, so no need to restart thonny on jdk install
-            os.environ['JAVA_HOME'] = str(pathlib.Path(THONNY_USER_DIR) / dest)
-
-            return f'JDK-{_REQUIRE_JDK} installed and JAVA_HOME set'
-
-        # set jdk path to thonny config directory
-        set_java_home(pathlib.Path(THONNY_USER_DIR) / jdk_dir)
-        return 'JAVA_HOME set'
+                src = pathlib.Path(THONNY_USER_DIR) / name
+                dest = pathlib.Path(THONNY_USER_DIR) / jdk_dir
+                os.rename(src, dest)
+                print('> rename extracted jdk')
+                break
+        # set java_home
+        print('> set java_home')
+        set_java_home(pathlib.Path(THONNY_USER_DIR) / dest)
+        os.environ['JAVA_HOME'] = str(pathlib.Path(THONNY_USER_DIR) / dest)
+        print('> done')
 
 
 class JdkDialog(ui_utils.CommonDialog):
@@ -96,7 +107,7 @@ class JdkDialog(ui_utils.CommonDialog):
           command=self._proceed,
           default='active'
         )
-        self.ok_button.grid(row=3, column=0, padx=15, pady=15, sticky='w')
+        self.ok_button.grid(row=2, column=0, padx=15, pady=15, sticky='w')
         self.ok_button.focus_set()
         # cancel button
         self.cancel_button = ttk.Button(
@@ -104,7 +115,7 @@ class JdkDialog(ui_utils.CommonDialog):
           text='Cancel',
           command=self._close
         )
-        self.cancel_button.grid(row=3, column=1, padx=15, pady=15, sticky='e')
+        self.cancel_button.grid(row=2, column=1, padx=15, pady=15, sticky='e')
 
 
     def _proceed(self, event=None) -> None:
@@ -112,15 +123,17 @@ class JdkDialog(ui_utils.CommonDialog):
         self.ok_button.destroy()
         self.cancel_button.destroy()
         # progress bar label
-        dl_label = ttk.Label(self.main_frame, text='Downloading ...')
-        dl_label.grid(row=2, columnspan=2, pady=(0,15))
+        msg = f'Downloading and extracting JDK {_REQUIRE_JDK} ...'
+        dl_label = ttk.Label(self.main_frame, text=msg)
+        dl_label.grid(row=1, columnspan=2, pady=(0,15))
         # progress bar
         self.progress_bar = ttk.Progressbar(
           self.main_frame, orient=tk.HORIZONTAL, mode='indeterminate')
         self.progress_bar.grid(
-          row=3, column=0, columnspan=2, sticky=tk.EW, padx=15, pady=(0,15))
-
-        self.start_downloading()
+          row=2, column=0, columnspan=2, padx=15, pady=(0,15), sticky='ew')
+        # start progress bar animation and download thread
+        self.main_frame.tkraise()
+        self.progress_bar.start(20)
         download_thread = DownloadJDK()
         download_thread.start()
         self.monitor(download_thread)
@@ -129,34 +142,19 @@ class JdkDialog(ui_utils.CommonDialog):
         '''cancel without starting jdk downloader thread'''
         self.destroy()
 
-    def start_downloading(self) -> None:
-        self.main_frame.tkraise()
-        self.progress_bar.start(20)
-
-    def stopped_downloading(self) -> None:
-        self.progress_bar.stop()
-
     def monitor(self, download_thread) -> None:
+        '''animate progress bar while jdk installs and extracts'''
         if download_thread.is_alive():
             self.after(100, lambda: self.monitor(download_thread))
         else:
-            self.stopped_downloading()
+            self.progress_bar.stop()
             self.destroy()
-            showinfo('title', 'done', master=get_workbench())
-            print('stop dling')
-
-
-def set_java_home(jdk_path: pathlib.Path) -> None:
-    '''add jdk path to config file (tools > options > general > env vars)'''
-    jdk_path = 'JAVA_HOME=' + str(jdk_path)
-    env_vars = get_workbench().get_option('general.environment')
-
-    if jdk_path not in env_vars:
-        env_vars.append(jdk_path)
-
-    get_workbench().set_option('general.environment', env_vars)
+            msg = f'JDK {_REQUIRE_JDK} extracted to {THONNY_USER_DIR}\n\n'
+            msg += 'You can now run py5 sketches.'
+            showinfo('JDK done', msg, master=get_workbench())
 
 
 def install_jdk() -> None:
-    '''call from where this module is imported'''
-    ui_utils.show_dialog(JdkDialog(get_workbench()))
+    '''call this function from where this module is imported'''
+    if not jdk_install_exists():
+        ui_utils.show_dialog(JdkDialog(get_workbench()))
