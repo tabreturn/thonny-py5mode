dbd0e33e9b51c9424158b06e595ce88e3eabcef0
tabreturn <github@tabreturn.com>
2022-07-19 15:54:06 +1200

    - remove pull-request comment for tkcolorpicker

--- thonnycontrib/thonny-py5mode/__init__.py
+++ thonnycontrib/thonny-py5mode/__init__.py
@@ -30,8 +30,7 @@ try:  # thonny 4 package layout
 except ImportError:  # thonny 3 package layout
     pass
 # modified tkcolorpicker (by j4321) to work with thonny for macos
-# https://github.com/tabreturn/thonny-py5mode-tkcolorpicker
-# hopefully, pull-request is accepted so this can install via pypi
+# fork: https://github.com/tabreturn/thonny-py5mode-tkcolorpicker
 from .py5colorpicker.tkcolorpicker import askcolor
 
 
