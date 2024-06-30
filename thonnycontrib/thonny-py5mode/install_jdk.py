985326b67fbaaf0dd83daf705e3ffcc23cc7da99
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-06-30 16:27:30 -0300

    jdk_install_exists() and set_java_home() changes
    These changes are necessary to complement another 
    __init__.py change that triggers a JAVA_HOME var update
    on each run of the portable version of Thonny.

--- thonnycontrib/thonny-py5mode/install_jdk.py
+++ thonnycontrib/thonny-py5mode/install_jdk.py
@@ -19,40 +19,43 @@ It\'ll need to download about 180 MB.'''
 
 
 def jdk_install_exists() -> bool:
-    '''check system for existing jdk that meets the py5 version requirements'''
+    '''
+    Look for a jdk in Thonny's user directory first, and if one is present,
+    set up JAVA_HOME, add it to Thonny's options, and return True. Otherwise,
+    look for existing JAVA_HOME pointing to a jdk that meets the py5 version
+    requirements and return True/False accordingly.
+    '''
+    # search for jdk in Thonny's user config directory
     jdk_dir = 'jdk-' + str(_REQUIRE_JDK)
-    system_jdk = 'TBD'
-
+    for name in os.listdir(THONNY_USER_DIR):
+        if name.startswith(jdk_dir):
+            # set JAVA_HOME in Thonny's config (and in current env) with jdk_dir path 
+            set_java_home(pathlib.Path(THONNY_USER_DIR) / jdk_dir)
+            return True    
     # check system for existing jdk install (and the version number)
+    system_jdk = 'TBD'
     if os.environ.get('JAVA_HOME') is not None:
         jdk_ver = os.environ['JAVA_HOME'].split('jdk-')[-1].split('.')[0]
         system_jdk = jdk_ver
-
-    for name in os.listdir(THONNY_USER_DIR):
-        # check for jdk in the thonny config directory
-        if name.startswith(jdk_dir):
-            # set jdk path to thonny config directory
-            set_java_home(pathlib.Path(THONNY_USER_DIR) / jdk_dir)
-            return True
-
-    if not system_jdk.isdigit() or int(system_jdk) < _REQUIRE_JDK:
-        return False
+    # False if no JAVA_HOME was set, or if it is not the required version
+    return system_jdk.isdigit() and int(system_jdk) >= _REQUIRE_JDK     
 
 
 def set_java_home(jdk_path: pathlib.Path) -> None:
-    '''add jdk path to config file (tools > options > general > env vars)'''
-    jdk_path = 'JAVA_HOME=' + str(jdk_path)
-    # if mac arm system, add /contents/home to jdk path
-    if str(jdk.OS) == 'mac' and str(jdk.ARCH) == 'arm':
-        jdk_path += '/Contents/Home'
-
+    '''
+    Add jdk path to current envirnonment and to Thonny's config file
+    as visible in (tools > options > general > env vars)
+    '''
+    os.environ['JAVA_HOME'] = str(pathlib.Path(THONNY_USER_DIR) / jdk_path)
+    java_home_var = 'JAVA_HOME=' + str(jdk_path)
     env_vars = get_workbench().get_option('general.environment')
-
-    if jdk_path not in env_vars:
-        env_vars.append(jdk_path)
-
+    for i, env_var in enumerate(env_vars):
+        if 'JAVA_HOME' in env_var:
+           env_vars[i] = java_home_var
+           break
+    else:
+        env_vars.append(java_home_var)
     get_workbench().set_option('general.environment', env_vars)
-
 
 class DownloadJDK(threading.Thread):
     def __init__(self):
@@ -157,3 +160,4 @@ def install_jdk() -> None:
     '''call this function from where this module is imported'''
     if not jdk_install_exists():
         ui_utils.show_dialog(JdkDialog(get_workbench()))
+
